5ddd5e1ca06b7d727367060b3b0e577451a5f8df
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2026-06-09 17:12:41 +0000

    Moved main content into Main

--- AaronsToDoApp.FrontEnd/src/App.tsx
+++ AaronsToDoApp.FrontEnd/src/App.tsx
@@ -1,18 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 import { Container } from '@mui/material';
 import Box from '@mui/material/Box';
 
 import { AuthProvider } from './components/auth/AuthContext';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import PublicOnlyRoute from './components/auth/PublicOnlyRoute';
 
 import Header from './components/Header';
+import Main from './components/Main';
 import Footer from './components/Footer';
-
-import TasksMain from './components/tasks/TasksMain';
-import Login from './components/users/Login';
-import Register from './components/users/Register';
 
 export default function App() {
   return (
@@ -21,29 +14,11 @@ export default function App() {
         display: 'flex', flexDirection: 'column', minHeight: '100vh'
       }}>
         <Header />
-        <Box component="main" sx={{ flexGrow: 1, py: 3 }}>
-          <Container maxWidth="md">
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={
-                  <ProtectedRoute>
-                    <TasksMain />
-                  </ProtectedRoute>
-                } />
-                <Route path="/login" element={
-                  <PublicOnlyRoute>
-                    <Login nextUrl='/' registerUrl='/register' />
-                  </PublicOnlyRoute>
-                } />
-                <Route path="/register" element={
-                  <PublicOnlyRoute>
-                    <Register loginUrl='/login' />
-                  </PublicOnlyRoute>
-                } />
-              </Routes>
-            </BrowserRouter>
-          </Container>
-        </Box>
+        <Container component="main" maxWidth="md">
+          <Box sx={{ flexGrow: 1, py: 3 }}>
+            <Main />
+          </Box>
+        </Container>
         <Footer />
       </Box>
     </AuthProvider>
---
+++ AaronsToDoApp.FrontEnd/src/components/Main.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import PublicOnlyRoute from "./auth/PublicOnlyRoute";
+import TasksMain from "./tasks/TasksMain";
+import Login from "./users/Login";
+import Register from "./users/Register";
+
+export default function Main() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <TasksMain />
+                    </ProtectedRoute>
+                } />
+                <Route path="/login" element={
+                    <PublicOnlyRoute>
+                        <Login nextUrl='/' registerUrl='/register' />
+                    </PublicOnlyRoute>
+                } />
+                <Route path="/register" element={
+                    <PublicOnlyRoute>
+                        <Register loginUrl='/login' />
+                    </PublicOnlyRoute>
+                } />
+            </Routes>
+        </BrowserRouter>
+    );
+}
